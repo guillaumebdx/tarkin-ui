fe6562ec610e408546fbf74d7f8d953190251c98
Guillaume HARARI <guillaumeharari@hotmail.com>
2018-12-20 00:16:11 +0100

    Material UI installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Icon from '@material-ui/core/Icon';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Icon>star</Icon>
+        <Button variant="contained" color="primary">
+        Hello World
+        </Button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
