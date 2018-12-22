fc3f0f3bc6b32d6e735d5c910247fa17bc596f73
Guillaume HARARI <guillaumeharari@hotmail.com>
2018-12-22 12:54:09 +0100

    #792468 Architecture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Icon from '@material-ui/core/Icon';
 import ReactDOM from 'react-dom';
@@ -46,6 +45,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <header className="App-header">
+      </header>
       <Wrapper>
       <Title>Test de styled component!</Title>
       </Wrapper>
@@ -62,20 +63,6 @@ class App extends Component {
         <Button variant="contained" color="primary">
         Hello World
         </Button>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
