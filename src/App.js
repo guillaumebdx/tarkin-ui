5e6bae523462fc5f7c237b98030541f928e16c08
Guillaume HARARI <guillaumeharari@hotmail.com>
2018-12-23 19:28:48 +0100

    #791892 cards on homepage without Grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
 import React, { Component }        from 'react';
 import                                  './App.css';
 import BottomBar                   from './components/Main/BottomBar';
+import MainCard                    from './components/Card/MainCard';
+import Grid                        from '@material-ui/core/Grid';
+import {Doughnut}                  from 'react-chartjs-2';
+const data = {
+        labels: [
+            'Immobilier',
+            'Financier',
+            'Passif'
+        ],
+        datasets: [{
+            data: [300, 50, 100],
+            backgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56'
+            ],
+            hoverBackgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56'
+            ],
+        }],
+       
+    };
+const options = {
+        legend: {
+            display: true,
+            position: 'right',
+            "labels": {
+                "fontColor": "rgb(255, 255, 255)"
+              }
+        },
+  };
+
 
 class App extends Component {
 
@@ -8,7 +42,32 @@ class App extends Component {
 	render() {
 	    return (
 	        <div className="App">
-	        	<BottomBar />
+	        
+	        	<MainCard 
+	        		title =     "Patrimoine" 
+	        		subHeader = "Composition du patrimoine"
+	        		data =      <Doughnut data={data} options = {options} />
+	        		collapse =  "Détail de votre patrimoine financier :"
+	        	/>
+	        	<MainCard 
+			        title =     "Profil financier" 
+			        subHeader = "Votre profil d'investisseur"
+			        collapse =  "Détail de votre profil financier :"
+			    />	
+	        	<MainCard 
+		        	title =     "Famille" 
+		        	subHeader = "Composition de la famille"
+		        	collapse =  "Détail de la composition :"
+		        />
+		        <MainCard 
+			        title =     "Diversification" 
+			        subHeader = "Diversification de votre patrimoine"
+			        collapse =  "Détail de la diversification :"
+			    />
+			    	
+		        
+		        <BottomBar />
+	        	
 	        </div>
 	    );
 	}
---
+++ src/components/Card/MainCard.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
+import AddIcon from '@material-ui/icons/Add';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const styles = theme => ({
+  card: {
+    maxWidth: 400,
+    
+  },
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+    marginLeft: 'auto',
+    [theme.breakpoints.up('sm')]: {
+      marginRight: -8,
+    },
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+});
+
+class RecipeReviewCard extends React.Component {
+  state = { expanded: false };
+
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Card className={"mainCard " + classes.card}>
+        <CardHeader
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={this.props.title}
+          subheader={this.props.subHeader}
+        />
+        <CardContent>
+        {this.props.data}
+        </CardContent>
+        <CardActions className={classes.actions} disableActionSpacing>
+          <IconButton>
+            <EditIcon />
+          </IconButton>
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+          <IconButton
+            className={classnames(classes.expand, {
+              [classes.expandOpen]: this.state.expanded,
+            })}
+            onClick={this.handleExpandClick}
+            aria-expanded={this.state.expanded}
+            aria-label="Voir plus"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+          	   {this.props.collapse}
+          </CardContent>
+        </Collapse>
+      </Card>
+    );
+  }
+}
+
+RecipeReviewCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RecipeReviewCard);
