553c63571cf17410e1b525c0eec600f7f8d08f2f
Guillaume HARARI <guillaumeharari@hotmail.com>
2019-01-05 00:41:40 +0100

    #792464 Family tree done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component }        from 'react';
 import                                  './App.css';
+import                                  './styles/familyTree.css';
 import BottomBar                   from './components/Main/BottomBar';
 import MainCard                    from './components/Card/MainCard';
 import { Grid, Row, Col }          from 'react-flexbox-grid';	
@@ -8,6 +9,7 @@ import OptionsRadar                from './const/OptionsRadar';
 import OptionsAsset                from './const/OptionsAsset';
 import DataAssetMock               from './Mocks/DataAssetMock';
 import DataRadarMock               from './Mocks/DataRadarMock';
+import FamilyTree                  from './components/Card/FamilyTree';
 
 
 
@@ -19,23 +21,12 @@ class App extends Component {
 		this.state = {
 				physicalPersons : [],
 		}
-		fetch("http://127.0.0.1:8000/api/user/339/physical-persons")
+		fetch("http://127.0.0.1:8000/api/user/345/physical-persons")
 		.then(response => response.json())
 		.then(data => this.setState({ physicalPersons: data }));
 	}
-	
-	
 
 	render() {
-		let physicalPersonsData = this.state.physicalPersons
-		const PhysicalPersons = () => {
-			const rows = physicalPersonsData.map((item, index) => 
-				<div key={index}>{item.first_name}</div>
-			);
-			 return <div className='offers'>{rows}</div>;
-		}
-		
-
 	    return (
 	        <div className="App">
 	        <div className="mainContainer">
@@ -43,33 +34,33 @@ class App extends Component {
 		            <Row around="md">
 		            <Col xs={12} md={6}>
 		        	<MainCard 
-		        		title =     "Patrimoine" 
+		        		title     = "Patrimoine" 
 		        		subHeader = "Composition du patrimoine"
-		        		data =      <Doughnut data={DataAssetMock} options = {OptionsAsset} />
-		        		collapse =  "Détail de votre patrimoine financier :"
+		        		data      = <Doughnut data={DataAssetMock} options = {OptionsAsset} />
+		        		collapse  = "Détail de votre patrimoine financier :"
 		        	/>
 		        	</Col>
 		        	<Col xs={12} md={6}>
 		        	<MainCard 
-				        title =     "Profil financier" 
+				        title     = "Profil financier" 
 				        subHeader = "Votre profil d'investisseur"
-				        data      =  <Radar data={DataRadarMock} options= {OptionsRadar} />
-				        collapse =  "Détail de votre profil financier :"
+				        data      = <Radar data={DataRadarMock} options= {OptionsRadar} />
+				        collapse  = "Détail de votre profil financier :"
 				    />	
 		        	</Col>
 		        	<Col xs={12} md={6}>
 				    <MainCard 
-			        	title =     "Famille" 
+			        	title     = "Famille" 
 			        	subHeader = {"Composition de la famille " } 
-				    	data = <PhysicalPersons />
-			        	collapse =  "Détail de la composition :"
+				    	data      = <FamilyTree personsData =  {this.state.physicalPersons} />
+			        	collapse  = "Détail de la composition :"
 			        />
 				       </Col>
 				       <Col xs={12} md={6}>
 				       <MainCard 
-					        title =     "Diversification" 
+					        title     = "Diversification" 
 					        subHeader = "Diversification de votre patrimoine"
-					        collapse =  "Détail de la diversification :"
+					        collapse  = "Détail de la diversification :"
 					    />
 					 </Col>
 					 </Row>
---
+++ src/components/Card/FamilyTree.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Icon                 from '@material-ui/core/Icon';
+
+class FamilyTree extends Component 
+{
+	
+	render() {
+		let physicalPersonsData = this.props.personsData
+		let children            = [];
+		let cradle              = [];
+		let spouse              = [];
+		physicalPersonsData.forEach(function(element) {
+			  if(element.family_position === 'Conjoint' && element.cradle === false) {
+				  spouse.push(element)
+			  }
+			  if(element.cradle === true) {
+				  cradle.push(element)
+			  }
+			});
+		physicalPersonsData.forEach(function(element) {
+			  if(element.family_position === 'Enfant') {
+				  children.push(element)
+			  }
+			});
+		
+		const PhysicalPersons = () => {
+			const childrenData = children.map((item, index) => 
+			<li  key={index}>
+				<div className="treeHover"><Icon fontSize="large">person_pin</Icon></div>
+				<div className="treeName">{item.first_name}</div>
+			</li>
+			
+		);
+			const cradleData = cradle.map((item, index) => 
+			<li  key={index}>
+				<div className="treeHover"><Icon fontSize="large">person_pin</Icon></div>
+				<div className="treeName">{item.first_name}</div>
+			</li>
+			
+		);
+			const spouseData = spouse.map((item, index) => 
+			<li  key={index}>
+				<div className="treeHover"><Icon fontSize="large">person_pin</Icon></div>
+				<div className="treeName">{item.first_name}</div>
+			</li>
+			
+		);
+			 return (
+					 <div className="tree familyTree">
+						 <ul>
+						 	{cradleData}
+						 <li>
+						 <ul>
+						 	{childrenData}
+						 </ul>
+						 </li>
+						 	{spouseData}
+						 </ul>
+					 </div>
+
+			 	
+		 		    )
+			 		
+		}
+		return (<PhysicalPersons />)
+	}
+
+}
+export default FamilyTree;
